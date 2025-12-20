f598d552c363fc87b85da2fa8632a2e76d09205c
d-luan <dluan1285@gmail.com>
2025-12-20 20:59:10 -0300

    feat: add learning about formatting and variables

---
+++ Chapter02/Formatting/Program.cs
@@ -0,0 +1,25 @@
+﻿int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+decimal totalPrice = pricePerApple * numberOfApples;
+
+//Console.WriteLine(
+//    format: "{0} apples cost {1:C}",
+//    arg0: numberOfApples,
+//    arg1: pricePerApple * numberOfApples);
+
+//string formatted = string.Format(
+//    format: "{0} apples cost {1:C}",
+//    arg0: numberOfApples,
+//    arg1: pricePerApple * numberOfApples);
+
+//Console.WriteLine(formatted);
+
+//string formatted2 = string.Format($"{numberOfApples} apples cost {totalPrice:C}");
+
+//Console.WriteLine(formatted2);
+
+Console.WriteLine("{0} {1} lived in {2}.",
+    arg0: "Roger", arg1: "Cevung", arg2: "Stockholm");
+
+Console.WriteLine("{0} lives in {1} and his favorite team is {2}. {2} will be champions in {3}!",
+    "John", "São Paulo", "Corinthians", 2026);
---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,47 @@
+﻿#region Storing any type of object
+
+object height = 1.88;
+object name = "Amir";
+Console.WriteLine($"{name} is {height} metres tall.");
+
+//int length1 = name.Length;
+int length2 = ((string)name).Length;
+Console.WriteLine($"{name} has {length2} characters.");
+
+#endregion
+
+#region Storing dynamic types.
+
+dynamic something;
+
+something = new[] { 3, 5, 7 };
+
+//something = 12;
+
+//something = "Ahmed";
+
+Console.WriteLine($"The length of something is {something.Length}");
+Console.WriteLine($"something is an {something.GetType()}");
+
+#endregion
+
+#region Inferring the type of a local variable
+
+
+
+#endregion
+
+
+#region Getting and setting the default values for types
+
+Console.WriteLine($"default(int) = {default(int)}");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+Console.WriteLine($"default(string) = {default(string)}");
+
+int number = 13;
+Console.WriteLine($"number set to: {number}");
+number = default;
+Console.WriteLine($"number reset to its default: {number}");
+
+#endregion
