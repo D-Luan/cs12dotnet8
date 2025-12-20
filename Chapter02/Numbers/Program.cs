1722a96e2de697f1883b274a98286b4166b190c4
d-luan <dluan1285@gmail.com>
2025-12-20 07:51:20 -0300

    chore: initial setup and files

---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8400;
+
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+Console.WriteLine($"{decimalNotation:N0}");
+Console.WriteLine($"{binaryNotation:N0}");
+Console.WriteLine($"{hexadecimalNotation:N0}");
+Console.WriteLine($"{decimalNotation:X}");
+Console.WriteLine($"{binaryNotation:X}");
+Console.WriteLine($"{hexadecimalNotation:X}");
+
+Console.WriteLine($"int usa {sizeof(int)}");
+Console.WriteLine($"double usa {sizeof(double)}");
+Console.WriteLine($"decimal usa {sizeof(decimal)}");
+
+Console.WriteLine("Usando double:");
+double a = 0.1;
+double b = 0.2;
+if (a + b == 0.3)
+{
+    Console.WriteLine($"{a} + {b} é igual a 0.3");
+}
+else
+{
+    Console.WriteLine($"{a} + {b} NÃO é igual a 0.3");
+}
+
+Console.WriteLine("Usando decimals:");
+decimal c = 0.1M;
+decimal d = 0.2M;
+if (c + d == 0.3M)
+{
+    Console.WriteLine($"{c} + {d} é igual a {0.3M}");
+}
+else
+{
+    Console.WriteLine($"{c} + {d} NÃO é igual a {0.3M}");
+}
---
+++ Chapter02/Variables/Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+
+//object height = 1.88;
+//object name = "Amir";
+//Console.WriteLine($"{name} tem {height} metros de altura.");
+
+//int length1 = name.Length;
+//int length2 = ((string)name).Length;
+//Console.WriteLine($"{name} tem {length2} caracteres.");
+
+//dynamic something;
+//something = new[] { 3, 5, 7 };
+//something = 12;
+//something = "Ahmed";
+//Console.WriteLine($"O comprimento de something é {something.Length}");
+//Console.WriteLine($"something é um {something.GetType()}");
+
+var population = 67_000_000;
+var weight = 1.88;
+var price = 4.99M;
+var fruit = "Apples";
+var letter = 'Z';
+var happy = true;
+
+var xml1 = new XmlDocument();
+XmlDocument xml2 = new XmlDocument();
+
+var file1 = File.CreateText("something1.txt");
+StreamWriter file2 = File.CreateText("something2.txt");
