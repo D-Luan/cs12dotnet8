ab075b3f6ae82b88e90c1bc60bba4f6b7562c6ca
d-luan <dluan1285@gmail.com>
2025-12-20 21:00:06 -0300

    feat: add exercises with numbers and vocabulary csharp

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -1,4 +1,8 @@
-﻿int decimalNotation = 2_000_000;
+﻿Console.WriteLine($"int uses {sizeof(int)} bytes");
+Console.WriteLine($"double uses {sizeof(double)} bytes");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes");
+
+int decimalNotation = 2_000_000;
 int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
 int hexadecimalNotation = 0x_001E_8400;
 
@@ -38,3 +42,41 @@ else
 {
     Console.WriteLine($"{c} + {d} NÃO é igual a {0.3M}");
 }
+
+#region Special float and double values
+
+Console.WriteLine($"double.Epsilon: {double.Epsilon}");
+Console.WriteLine($"double.Epsilon to 324 decimal places: {double.Epsilon:N324}");
+Console.WriteLine($"double.Epsilon to 330 decimal places: {double.Epsilon:N330}");
+
+const int col1 = 37;
+const int col2 = 6;
+string line = new string('-', col1 + col2 + 3);
+
+Console.WriteLine(line);
+Console.WriteLine($"{"Expression",-col1} | {"Value",col2}");
+Console.WriteLine(line);
+Console.WriteLine($"{"double.NaN",-col1} | {double.NaN,col2}");
+Console.WriteLine($"{"double.PositiveInfinity",-col1} | {double.PositiveInfinity,col2}");
+Console.WriteLine($"{"double.NegativeInfinity",-col1} | {double.NegativeInfinity,col2}");
+Console.WriteLine(line);
+Console.WriteLine($"{"0.0 / 0.0",-col1} | {0.0 / 0.0,col2}");
+Console.WriteLine($"{"3.0 / 0.0",-col1} | {3.0 / 0.0,col2}");
+Console.WriteLine($"{"-3.0 / 0.0",-col1} | {-3.0 / 0.0,col2}");
+Console.WriteLine($"{"3.0 / 0.0 == double.PositiveInfinity",-col1} | {3.0 / 0.0 == double.PositiveInfinity,col2}");
+Console.WriteLine($"{"-3.0 / 0.0 == double.NegativeInfinity",-col1} | {-3.0 / 0.0 == double.NegativeInfinity,col2}");
+Console.WriteLine($"{"0.0 / 3.0",-col1} | {0.0 / 3.0,col2}");
+Console.WriteLine($"{"0.0 / -3.0",-col1} | {0.0 / -3.0,col2}");
+Console.WriteLine(line);
+
+#endregion
+
+#region New number types and unsafe code
+
+unsafe
+{
+    Console.WriteLine($"Half uses {sizeof(Half)} bytes and can store numbers in the range {Half.MinValue:N0} to {Half.MaxValue:N0}.");
+    Console.WriteLine($"Int128 uses {sizeof(Int128)} bytes and can store numbers in the range {Int128.MinValue:N0} to {Int128.MaxValue:N0}.");
+}
+
+#endregion
