892d47824bd8b2cec2ae7ba9076b73e12baf7634
d-luan <dluan1285@gmail.com>
2026-01-07 18:04:07 -0300

    feat: add learning about properties, methods, indexers and more from Chapter 05

--- Chapter05/PacktLibraryNetStandard2/Person.cs
+++ Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -1,11 +1,101 @@
 ﻿namespace Packt.Shared;
 
-public class Person : object
+public partial class Person : object
 {
     #region Fields: Data or state for this person.
+
     public string? Name;
     public DateTimeOffset Born;
+    // This has been moved to PersonAutoGen.cs as a property.
+    //public WondersOfTheAncientWorld FavoriteAncientWonder;
     public WondersOfTheAncientWorld BucketList;
+    public List<Person> Children = new();
+    public const string Species = "Homo Sapiens";
+    public readonly string HomePlanet = "Earth";
+    public readonly DateTime Instantiated;
 
     #endregion
+
+    #region Constructors: Called when using new to instantiate a type.
+
+    public Person()
+    {
+        // Constructors can set default values for fields
+        // including any read-only fields like Instantiated.
+        Name = "Unknown";
+        Instantiated = DateTime.Now;
+    }
+
+    public Person(string initialName, string homePlanet)
+    {
+        Name = initialName;
+        HomePlanet = homePlanet;
+        Instantiated = DateTime.Now;
+    }
+
+    #endregion
+
+    public static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException(
+                $"{nameof(number)} cannot be less than zero.");
+        }
+
+        return localFactorial(number);
+
+        int localFactorial(int localNumber)
+        {
+            if (localNumber == 0) return 1;
+            return localNumber * localFactorial(localNumber - 1);
+        }
+    }
+
+    public void Deconstruct(out string? name,
+        out DateTimeOffset dob)
+    {
+        name = Name;
+        dob = Born;
+    }
+
+    public void Deconstruct(out string? name,
+        out DateTimeOffset dob,
+        out WondersOfTheAncientWorld fav)
+    {
+        name = Name;
+        dob = Born;
+        fav = FavoriteAncientWonder;
+    }
+
+    public (string, int) GetNameFruit()
+    {
+        return ("Banana", 9);
+    }
+
+    public (string, int) GetFruit()
+    {
+        return ("Apples", 5);
+    }
+
+    public void PassingParameters(int w, in int x, ref int y, out int z)
+    {
+        z = 100;
+        w++;
+        y++;
+        z++;
+
+        WriteLine($"In the method: w{w}, x={x}, y={y}, z={z}");
+    }
+
+    public string SayHello()
+    {
+        return $"{Name} says 'Hello'!";
+    }
+
+    public string SayHello(string name)
+    {
+        return $"{Name} says 'Hello, {name}!'";
+    }
+
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Packt.Shared;
+using System.Reflection.Metadata;
+using Fruit = (string Name, int Number);
 
-ConfigureConsole();
+//ConfigureConsole();
 
 Person bob = new();
 //WriteLine(bob);
@@ -12,8 +14,8 @@ bob.Born = new DateTimeOffset(
     hour: 16, minute: 28, second: 0,
     offset: TimeSpan.FromHours(-5));
 
-WriteLine(format: "{0} was born on {1:D}.",
-    arg0: bob.Name, arg1: bob.Born);
+//WriteLine(format: "{0} was born on {1:D}.",
+//    arg0: bob.Name, arg1: bob.Born);
 
 //bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
 
@@ -23,18 +25,216 @@ WriteLine(format: "{0} was born on {1:D}.",
 //    arg1: bob.FavoriteAncientWonder,
 //    arg2: (int)bob.FavoriteAncientWonder);
 
-bob.BucketList =
-        WondersOfTheAncientWorld.HangingGardensOfBabylon
-        | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+//bob.BucketList =
+//        WondersOfTheAncientWorld.HangingGardensOfBabylon
+//        | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
 
-WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+//WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
 
-Person alice = new()
+//Person alice = new()
+//{
+//    Name = "Alice Jones",
+//    Born = new(1998, 3, 7, 16, 28, 0,
+//        TimeSpan.Zero)
+//};
+
+//WriteLine(format: "{0} was born on {1:d}.",
+//    arg0: alice.Name, arg1: alice.Born);
+
+////Works with all versions of C#
+//Person alfred = new Person();
+//alfred.Name = "Alfred";
+//bob.Children.Add(alfred);
+
+//// Works with C# 3 and later.
+//bob.Children.Add(new Person { Name = "Bella" });
+
+//// Works with C# 9 and Later.
+//bob.Children.Add(new() { Name = "Zoe" });
+
+//WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+
+//for (int childIndex = 0; childIndex < bob.Children.Count; childIndex++)
+//{
+//    WriteLine($"> {bob.Children[childIndex].Name}");
+//}
+
+//foreach (Person children in bob.Children)
+//{
+//    WriteLine($"> {children.Name}");
+//}
+
+//BankAccount.InterestRate = 0.012M;
+
+//BankAccount jonesAccount = new();
+//jonesAccount.AccountName = "Mrs. Jones";
+//jonesAccount.Balance = 2400;
+//WriteLine(format: "{0} earned {1:C} interest.",
+//    arg0: jonesAccount.AccountName,
+//    arg1: jonesAccount.Balance * BankAccount.InterestRate);
+
+//BankAccount gerrierAccount = new();
+//gerrierAccount.AccountName = "Ms. Gerrier";
+//gerrierAccount.Balance = 98;
+//WriteLine(format: "{0} earned {1:C} interest.",
+//    arg0: gerrierAccount.AccountName,
+//    arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+
+//WriteLine($"{bob.Name} is a {Person.Species}.");
+//WriteLine($"{bob.Name} was born on {bob.HomePlanet}.");
+
+//Person blankPerson = new();
+//WriteLine(format:
+//    "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+//    arg0: blankPerson.Name,
+//    arg1: blankPerson.HomePlanet,
+//    arg2: blankPerson.Instantiated);
+
+//Person gunny = new(initialName: "Gunny", homePlanet: "Mars");
+//WriteLine(format:
+//    "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+//    arg0: gunny.Name,
+//    arg1: gunny.HomePlanet,
+//    arg2: gunny.Instantiated);
+
+//Book book = new()
+//{
+//    Isbn = "978-1803237800",
+//    Title = "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals"
+//};
+
+//WriteLine("{0}: {1} written by {2} has {3:N0} pages.",
+//    book.Isbn, book.Title, book.Author, book.PageCount);
+
+//Book book = new(
+//    isbn: "978-1803237800",
+//    title: "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals")
+//{
+//    Author = "Mark J. Price",
+//    PageCount = 821
+//};
+
+//WriteLine("{0}: {1} written by {2} has {3:N0} pages.",
+//    book.Isbn, book.Title, book.Author, book.PageCount);
+
+//WriteLine(bob.SayHello());
+//WriteLine(bob.SayHello("Emily"));
+
+//int a = 10;
+//int b = 20;
+//int c = 30;
+//int d = 40;
+
+//WriteLine($"Before: a={a}, b={b}, c={c}, d={d}");
+
+//bob.PassingParameters(a, b, ref c, out d);
+
+//WriteLine($"After: a={a}, b={b}, c={c}, d={d}");
+
+//int e = 50;
+//int f = 60;
+//int g = 70;
+
+//WriteLine($"Before: e={e}, f={f}, g={g}, h doesn't exist yet!");
+
+//bob.PassingParameters(e, f, ref g, out int h);
+
+//WriteLine($"After: e={e}, f={f}, g={g}, h={h}");
+
+//(string, int) fruit = bob.GetFruit();
+//WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+
+//var thing1 = ("Neville", 4);
+//WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
+
+//var thing2 = (bob.Name, bob.Children.Count);
+//WriteLine($"{thing2.Name} has {thing2.Count} children.");
+
+//Fruit fruitNamed = bob.GetNameFruit();
+
+//Console.WriteLine(fruitNamed);
+
+//(string name, int number) namedFields = bob.GetNameFruit();
+
+//WriteLine($"{namedFields.name}, {namedFields.number}");
+
+//(string name, int number) = bob.GetNameFruit();
+
+//WriteLine($"{name}, {number}");
+
+//(string fruitName, int fruitNumber) = bob.GetFruit();
+//WriteLine($"Deconstructed tuple: {fruitName}, {fruitNumber}");
+
+//var (name1, dob1) = bob;
+//WriteLine($"Deconstructed person: {name1}, {dob1}");
+
+//var (name2, dob2, fav2) = bob;
+//WriteLine($"Deconstructed person: {name2}, {dob2}, {fav2}");
+
+//int number = -1;
+
+//try
+//{
+//    WriteLine($"{number}! is {Person.Factorial(number)}");
+//}
+//catch (Exception ex)
+//{
+//    WriteLine($"{ex.GetType()} says: {ex.Message} number was {number}.");
+//}
+
+Person sam = new()
 {
-    Name = "Alice Jones",
-    Born = new(1998, 3, 7, 16, 28, 0,
-        TimeSpan.Zero)
+    Name = "Sam",
+    Born = new(1969, 6, 25, 0, 0, 0, TimeSpan.Zero)
 };
 
-WriteLine(format: "{0} was born on {1:d}.",
-    arg0: alice.Name, arg1: alice.Born);
+//WriteLine(sam.Origin);
+//WriteLine(sam.Greeting);
+//WriteLine(sam.Age);
+
+//sam.FavoriteIceCream = "Chocolate Fudge";
+//WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}.");
+
+//string color = "black";
+
+//try
+//{
+//    sam.FavoritePrimaryColor = color;
+//    WriteLine($"Sam's favorite primary color is {sam.
+//        FavoritePrimaryColor}.");
+//}
+//catch (Exception ex)
+//{
+//    WriteLine("Tried to set {0} to '{1}': {2}",
+//        nameof(sam.FavoritePrimaryColor), color, ex.Message);
+//}
+
+//bob.FavoriteAncientWonder =
+//    WondersOfTheAncientWorld.StatueOfZeusAtOlympia |
+//    WondersOfTheAncientWorld.GreatPyramidOfGiza;
+
+//bob.FavoriteAncientWonder = (WondersOfTheAncientWorld)128;
+
+//bob.FavoriteAncientWonder = WondersOfTheAncientWorld.GreatPyramidOfGiza;
+
+//Console.WriteLine(bob.FavoriteAncientWonder);
+
+sam.Children.Add(new()
+{
+    Name = "Charlie",
+    Born = new(2010, 3, 18, 0, 0, 0, TimeSpan.Zero)
+});
+
+sam.Children.Add(new()
+{
+    Name = "Ella",
+    Born = new(2020, 12, 24, 0, 0, 0, TimeSpan.Zero)
+});
+
+WriteLine($"Sam's first child is {sam.Children[0].Name}.");
+WriteLine($"Sam's second child is {sam.Children[1].Name}.");
+
+WriteLine($"Sam's first child is {sam[0].Name}.");
+WriteLine($"Sam's second child is {sam[1].Name}.");
+
+WriteLine($"Sam's child named Ella is {sam["Ella"].Age} years old.");
