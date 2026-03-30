80e0f16e54c82ba1b491a0c38a152e2ebf20362f
d-luan <dluan1285@gmail.com>
2026-03-29 21:26:46 -0300

    feat: add learning about Environment and Filesytem

---
+++ Chapter09/WorkingWithFileSystems/Program.cs
@@ -0,0 +1,186 @@
+﻿using Packt.Shared;
+using Spectre.Console;
+using System.Xml;
+
+#region Handling cross-platform environments and filesystems
+
+SectionTitle("Hanling cross-platform environments and filesystems");
+
+Table table = new();
+
+table.AddColumn("[blue]MEMBER[/]");
+table.AddColumn("[blue]VALUE[/]");
+
+table.AddRow("Path.PathSeparator", PathSeparator.ToString());
+table.AddRow("Path.DirectorySeparatorChar", DirectorySeparatorChar.ToString());
+table.AddRow("Directory.GetCurrentDirectory", GetCurrentDirectory());
+table.AddRow("Environment.CurrentDirectory", CurrentDirectory);
+table.AddRow("Environment.SystemDirectory", SystemDirectory);
+table.AddRow("Path.GetTempPath", GetTempPath());
+table.AddRow("");
+table.AddRow("GetFolderPath(SpecialFolder", "");
+table.AddRow("  .System", GetFolderPath(SpecialFolder.System));
+table.AddRow("  .ApplicationData", GetFolderPath(SpecialFolder.ApplicationData));
+table.AddRow("  .MyDocuments", GetFolderPath(SpecialFolder.MyDocuments));
+table.AddRow("  .Personal", GetFolderPath(SpecialFolder.Personal));
+
+AnsiConsole.Write(table);
+
+#endregion
+
+SectionTitle("Managing drives");
+
+Table drives = new();
+
+drives.AddColumn("[blue]NAME[/]");
+drives.AddColumn("[blue]TYPE[/]");
+drives.AddColumn("[blue]FORMAT[/]");
+drives.AddColumn(new TableColumn("[blue]SIZE (BYTES)[/]").RightAligned());
+drives.AddColumn(new TableColumn("[blue]FREE SPACE[/]").RightAligned());
+
+foreach (DriveInfo drive in DriveInfo.GetDrives())
+{
+    if (drive.IsReady)
+    {
+        drives.AddRow(drive.Name, drive.DriveType.ToString(),
+            drive.DriveFormat, drive.TotalSize.ToString("N0"),
+            drive.AvailableFreeSpace.ToString("N0"));
+    }
+    else
+    {
+        drives.AddRow(drive.Name, drive.DriveType.ToString(),
+            string.Empty, string.Empty, string.Empty);
+    }
+}
+
+AnsiConsole.Write(drives);
+
+SectionTitle("Managing directories");
+
+string newFolder = Combine(GetFolderPath(SpecialFolder.Personal), "NewFolder");
+
+WriteLine($"Working with: {newFolder}");
+
+WriteLine($"Does it exist? {Path.Exists(newFolder)}");
+
+WriteLine("Creating it...");
+CreateDirectory(newFolder);
+
+WriteLine($"Does it exist? {Directory.Exists(newFolder)}");
+Write("Confirm the directory exists, and then press any key.");
+ReadKey(intercept: true);
+
+WriteLine("Deleting it...");
+Delete(newFolder, recursive: true);
+WriteLine($"Does it exist? {Path.Exists(newFolder)}");
+
+SectionTitle("Managing files");
+
+string dir = Combine(GetFolderPath(SpecialFolder.Personal), "OutputFiles");
+
+CreateDirectory(dir);
+
+string textFile = Combine(dir, "Dummy.txt");
+string backupFile = Combine(dir, "Dummy.bak");
+WriteLine($"Working with: {textFile}");
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+StreamWriter textWriter = File.CreateText(textFile);
+textWriter.WriteLine("Hello, C#!");
+textWriter.Close();
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+File.Copy(sourceFileName: textFile, destFileName: backupFile, overwrite: true);
+
+WriteLine($"Does {backupFile} exist? {File.Exists(backupFile)}");
+
+Write("Confirm the files exist, and then press any key.");
+ReadKey(intercept: true);
+
+File.Delete(textFile);
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+WriteLine($"Reading contents of {backupFile}:");
+StreamReader textReader = File.OpenText(backupFile);
+WriteLine(textReader.ReadToEnd());
+textReader.Close();
+
+SectionTitle("Managing paths");
+
+WriteLine($"Folder Name: {GetDirectoryName(textFile)}");
+WriteLine($"File Name: {GetFileName(textFile)}");
+WriteLine("File Name without Extension: {0}", GetFileNameWithoutExtension(textFile));
+WriteLine($"File Extension: {GetExtension(textFile)}");
+WriteLine($"Random File Name: {GetRandomFileName()}");
+WriteLine($"Temporary File Name: {GetTempFileName()}");
+
+SectionTitle("Getting file information");
+
+FileInfo info = new(backupFile);
+WriteLine($"{backupFile}:");
+WriteLine($"    Contains {info.Length} bytes.");
+WriteLine($"    Last accessed: {info.LastAccessTime}");
+WriteLine($"    Has readonly set to {info.IsReadOnly}");
+
+FileInfo info = new(backupFile);
+WriteLine("Is the backup file compressed? {0}",
+    info.Attributes.HasFlag(FileAttributes.Compressed));
+
+SectionTitle("Writing to text streams");
+
+string textFile = Combine(CurrentDirectory, "streams.txt");
+
+StreamWriter text = File.CreateText(textFile);
+
+foreach (string item in Viper.Callsigns)
+{
+    text.WriteLine(item);
+}
+text.Close();
+
+OutputFileInfo(textFile);
+
+SectionTitle("Writing to XML streams");
+
+string xmlFile = Combine(CurrentDirectory, "streams.xml");
+
+FileStream? xmlFileStream = null;
+XmlWriter? xml = null;
+
+try
+{
+    xmlFileStream = File.Create(xmlFile);
+
+    xml = XmlWriter.Create(xmlFileStream, new XmlWriterSettings { Indent = true });
+
+    xml.WriteStartDocument();
+
+    xml.WriteStartElement("callsigns");
+
+    foreach (string item in Viper.Callsigns)
+    {
+        xml.WriteElementString("callsign", item);
+    }
+
+    xml.WriteEndElement();
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+finally
+{
+    if (xml is not null)
+    {
+        xml.Close();
+        WriteLine("The XML writer`s unmanaged resources have been disposed.");
+    }
+
+    if (xmlFileStream is not null)
+    {
+        xmlFileStream.Close();
+        WriteLine("The file stream`s unmanaged resources have been disposed.");
+    }
+}
+
+OutputFileInfo(xmlFile);
---
+++ Chapter09/WorkingWithFileSystems/Viper.cs
@@ -0,0 +1,10 @@
+﻿namespace Packt.Shared;
+
+public static class Viper
+{
+    public static string[] Callsigns = new[]
+    {
+        "Husker", "Starbuck", "Apollo", "Boomer",
+        "Bulldog", "Athena", "Helo", "Racetrack"
+    };
+}
