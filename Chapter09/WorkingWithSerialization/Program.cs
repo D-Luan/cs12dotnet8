9c2b01416d7d10d790a1042c2795a0034ed997c7
d-luan <dluan1285@gmail.com>
2026-04-08 23:11:23 -0300

    feat: add learning about serialization with XML and JSON

---
+++ Chapter09/WorkingWithSerialization/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Xml.Serialization;
+using Packt.Shared;
+using FastJson = System.Text.Json.JsonSerializer;
+
+List<Person> people = new()
+{
+    new(initialSalary: 30_000M)
+    {
+        FirstName = "Alice",
+        LastName = "Smith",
+        DateOfBirth = new(year: 1974, month: 3, day: 14)
+    },
+    new(initialSalary: 40_000M)
+    {
+        FirstName = "Bob",
+        LastName = "Jones",
+        DateOfBirth = new(year: 1969, month: 11, day: 23)
+    },
+    new(initialSalary: 20_000M)
+    {
+        FirstName = "Charlie",
+        LastName = "Cox",
+        DateOfBirth = new(year: 1984, month: 5, day: 4),
+        Children = new()
+        {
+            new(initialSalary: 0M)
+            {
+                FirstName = "Sally",
+                LastName = "Cox",
+                DateOfBirth = new(year: 2012, month: 7, day: 12)
+            }
+        }
+    }
+};
+
+SectionTitle("Serializing as XML");
+
+XmlSerializer xs = new(type: people.GetType());
+
+string path = Combine(CurrentDirectory, "people.xml");
+
+using (FileStream stream = File.Create(path))
+{
+    xs.Serialize(stream, people);
+}
+
+OutputFileInfo(path);
+
+SectionTitle("Deserializing XML files");
+
+using (FileStream xmlLoad = File.Open(path, FileMode.Open))
+{
+    List<Person>? loadedPeople =
+        xs.Deserialize(xmlLoad) as List<Person>;
+
+    if (loadedPeople is not null)
+    {
+        foreach (Person p in loadedPeople)
+        {
+            WriteLine("{0} has {1} children.",
+                p.LastName, p.Children?.Count ?? 0);
+        }
+    }
+}
+
+SectionTitle("Serializing with JSON");
+
+string jsonPath = Combine(CurrentDirectory, "people.json");
+
+using (StreamWriter jsonStream = File.CreateText(jsonPath))
+{
+    Newtonsoft.Json.JsonSerializer jss = new();
+
+    jss.Serialize(jsonStream, people);
+}
+
+OutputFileInfo(jsonPath);
+
+SectionTitle("Deserializing JSON files");
+
+await using (FileStream jsonLoad = File.Open(jsonPath, FileMode.Open))
+{
+    List<Person>? loadedPeople =
+        await FastJson.DeserializeAsync(utf8Json: jsonLoad,
+            returnType: typeof(List<Person>)) as List<Person>;
+
+    if (loadedPeople is not null)
+    {
+        foreach (Person p in loadedPeople)
+        {
+            WriteLine("{0} has {1} children.",
+                p.LastName, p.Children?.Count ?? 0);
+        }
+    }
+}
