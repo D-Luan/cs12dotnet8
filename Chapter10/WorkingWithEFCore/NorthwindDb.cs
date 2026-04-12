ed45842380736ce76bbfc4b47604233591150f39
d-luan <dluan1285@gmail.com>
2026-04-11 23:37:22 -0300

    feat: add learning about EF Core and models

---
+++ Chapter10/WorkingWithEFCore/NorthwindDb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.EntityModels;
+
+public class NorthwindDb : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string databaseFile = "Northwind.db";
+        string path = Path.Combine(Environment.CurrentDirectory, databaseFile);
+        string connectionString = $"Data Source={path}";
+        WriteLine($"Connection: {connectionString}");
+        optionsBuilder.UseSqlite(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .Property(category => category.CategoryName)
+            .IsRequired()
+            .HasMaxLength(15);
+
+        if (Database.ProviderName?.Contains("Sqlite") ?? false)
+        {
+            // To "fix" the lack of decimal support in SQLite.
+            modelBuilder.Entity<Product>()
+                .Property(product => product.Cost)
+                .HasConversion<double>();
+        }
+    }
+}
