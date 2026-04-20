0e2db4ca7aed472fac10d16dee2617091971d552
d-luan <dluan1285@gmail.com>
2026-04-19 23:41:26 -0300

    feat: add learning about repository and API RESTful

--- PracticalApps/Northwind.WebApi/Program.cs
+++ PracticalApps/Northwind.WebApi/Program.cs
@@ -1,15 +1,42 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Northwind.EntityModels;
+using Microsoft.Extensions.Caching.Memory;
+using Northwind.WebApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddSingleton<IMemoryCache>(
+    new MemoryCache(new MemoryCacheOptions()));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+builder.Services.AddControllers(options =>
+{
+    WriteLine("Default output formatters:");
+    foreach (IOutputFormatter formatter in options.OutputFormatters)
+    {
+        OutputFormatter? mediaFormatter = formatter as OutputFormatter;
+        if (mediaFormatter is null)
+        {
+            WriteLine($"    {formatter.GetType().Name}");
+        }
+        else
+        {
+            WriteLine(" {0}, Media types: {1}",
+                arg0: mediaFormatter.GetType().Name,
+                arg1: string.Join(", ", mediaFormatter.SupportedMediaTypes));
+        }
+    }
+})
+.AddXmlDataContractSerializerFormatters()
+.AddXmlSerializerFormatters();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddNorthwindContext();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
